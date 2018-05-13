358627657dc133419339d4adbc48abc880f339fd
Michael Poon <michaelkm.poon@mail.utoronto.ca>
2018-05-13 03:33:33 -0400

    further development on spaceship classes, added defend function

--- question_6/space_battle.py
+++ question_6/space_battle.py
@@ -1,3 +1,5 @@
+import random
+
 class Ship:
     """
     This is the main Ship class for all ships, including
@@ -12,9 +14,58 @@ class Ship:
         self.shield = shield
         self.hull = hull
 
+    def defend(self, damage, shield, hull):
+        if shield > 0:  # shield still exists
+            shield -= damage
+            if shield < 0:  # if damage breaks shield
+                hull += 0.5 * shield
+                shield = 0
+        else:
+            hull += 0.5 * shield  # shield at 0, only hull takes hit
+        return shield, hull
 
-class Warship(Ship)
+
+class Warship(Ship):
     """
     Warships additionally have high powered missiles which 
     fire 30% of the time.
     """
+
+    def __init__(self, name, laser, shield, hull, missiles):
+        super().__init__(name, laser, shield, hull)
+        self.missiles = missiles
+
+
+class Speeder(Ship):
+    """
+    Speeders additionally dodge 50% of incoming attacks.
+    """
+
+    def __init__(self, name, laser, shield, hull, chance):
+        super().__init__(name, laser, shield, hull)
+        self.chance = chance
+
+
+def update(name, shield, hull):
+    if hull <= 0:
+        print(name, " has been destroyed.")
+        return 0
+    else:
+        print(name, "has not been destroyed. Shield:", shield, "Hull:", hull)
+
+
+ShipA = Ship("Alfred", 30, 100, 100)  # Ships generated.
+ShipB = Ship("Betty", 30, 100, 100)
+ShipC = Ship("Chris", 30, 100, 100)
+ShipD = Warship("Destroyer", 30, 100, 100, 50)
+ShipE = Speeder("Eager", 30, 100, 100, 0.5)
+
+ShipA.shield, ShipA.hull = ShipA.defend(ShipB.laser, ShipA.shield, ShipA.hull)
+
+print(ShipA.shield, ShipA.hull)
+
+update(ShipA.name, ShipA.shield, ShipA.hull)
+
+
+
+
