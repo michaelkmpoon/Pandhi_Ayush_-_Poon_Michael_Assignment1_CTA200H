6388d6df57768cd58b75ebd78f938dd977a1a207
Michael Poon <michaelkm.poon@mail.utoronto.ca>
2018-05-13 18:44:14 -0400

    Finished main program and included sample battle log as a .txt file

--- question_6/space_battle.py
+++ question_6/space_battle.py
@@ -14,7 +14,9 @@ class Ship:
         self.shield = shield
         self.hull = hull
 
-    def defend(self, damage, shield, hull):
+    def defend(self, defender, damage, shield, hull):
+        if isinstance(defender, Speeder):
+            damage = damage * defender.dodge_attack()  # check if speeder dodges attack
         if shield > 0:  # shield still exists
             shield -= damage
             if shield < 0:  # if damage breaks shield
@@ -54,38 +56,73 @@ class Speeder(Ship):
         return 1
 
 
-def update(name, shield, hull):
+def update(ship, shield, hull):
     if hull <= 0:
-        print(name, " has been destroyed.")
+        print(ship.name, "has been destroyed.", "\n")
+        alive_list.remove(ship)
+        alive_list_name.remove(ship.name)
+
         return 0
     else:
-        print(name, "has not been destroyed. Shield:", shield, "Hull:", hull)
+        print(ship.name, "has not been destroyed. Shield:", shield, "Hull:", hull, "\n")
+        return 1
 
 
 def diagnostic(spaceship):
     print("///DIAGNOSTIC FOR:", spaceship.name)
     print("/ Laser Power:", spaceship.laser, "/ Shield:", spaceship.shield, "/ Hull:", spaceship.hull, "/")
-    print("Type:", type(spaceship))
+    print("Type:", type(spaceship), "\n")
 
-def battle_over(A, B, C, D, E):  #  each letter refers to a ship
-    total = [A, B, C, D, E]
-    death_count = 0
-    for each in total:  # check how many ships are destroyed
-        if each.hull == 0:
-            death_count += 1
-        if death_count == 4:
-            return 0
+
+def battle_over(total):  # each letter refers to a ship
+    if len(total) == 1:
+        print(total[0].name, "has won!")
+        return 0
+    return 1
+
+
+def random_fight(alive):
+
+    ship_index_attack = random.randint(0, len(alive) - 1)  # choose any of the alive ships
+    attacker = alive[ship_index_attack]
+    re_add = alive.pop(ship_index_attack)
+    ship_index_defend = random.randint(0, len(alive) - 1)  # choose any non-attacking alive ship
+    defender = alive[ship_index_defend]
+    alive.append(re_add)  # re-add attacker to list of alive ships
+    print(attacker.name, "attacks", defender.name, "\n")
+    return defender, shot(attacker) * attacker.laser
+
+
+def shot(attacker):
+    if isinstance(attacker, Warship):
+        return attacker.high_powered_missile()
+    else:
         return 1
 
-ShipA = Ship("Alfred", 30, 100, 100)  # Ships generated.
-ShipB = Ship("Betty", 30, 100, 100)
-ShipC = Ship("Chris", 30, 100, 100)
-ShipD = Warship("Destroyer", 30, 100, 100)
-ShipE = Speeder("Eager", 30, 100, 100)
 
-print("Let the games begin!")
-while battle_over(ShipA, ShipB, ShipC, ShipD, ShipE) == 1:
-    print("done")
+if __name__ == "__main__":
+
+    ShipA = Ship("Alfred", 30, 100, 4)  # Ships generated.
+    ShipB = Ship("Betty", 30, 100, 4)
+    ShipC = Ship("Chris", 30, 100, 4)
+    ShipD = Warship("Destroyer", 30, 100, 4)
+    ShipE = Speeder("Eager", 30, 100, 4)
+
+    alive_list = [ShipA, ShipB, ShipC, ShipD, ShipE]
+    alive_list_name = [ShipA.name, ShipB.name, ShipC.name, ShipD.name, ShipE.name]
+    day = 1
+    print("Let the games begin! \n ")
+
+    while battle_over(alive_list) == 1:  # main code, battle occurs with one attack per day
+        print("Day", day, "// Alive:", alive_list_name)
+        defending_ship, damage = random_fight(alive_list)
+        defending_ship.shield, defending_ship.hull = Ship.defend(defending_ship,
+            defending_ship, damage, defending_ship.shield, defending_ship.hull)
+        if update(defending_ship, defending_ship.shield, defending_ship.hull) == 1:  # check if a fatal hit has occurred
+            diagnostic(defending_ship)
+        day += 1
+
+    print("Battle over.")
 
 
 
